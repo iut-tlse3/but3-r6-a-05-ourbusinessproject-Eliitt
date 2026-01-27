2683a7a383aa641c6edf7c6bd59eb97c109432b0
NCL5182A <eliott.nee-chirol@etu.iut-tlse3.fr>
2026-01-27 11:37:06 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,19 +1,34 @@
 package ourbusinessproject;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
+@Entity
 public class Project {
 
-    @NotNull
-    @Size(min=1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
     private String title;
     private String description;
 
+    /**
+     * Set the title
+     * @param title the title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Set the description
+     * @param description the description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
